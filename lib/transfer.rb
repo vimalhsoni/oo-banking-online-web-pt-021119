b98e9ed031047ffcaa47a5aace1a7f5a063566c4
Vimal Soni <vimal.h.soni@gmail.com>
2019-03-29 23:53:13 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,6 +1,6 @@
 class Transfer
   # your code here
-  attr_accessor :sender, :receiver, :amount
+  attr_accessor :sender, :receiver, :amount, :status
   #
   def initialize(sender,receiver,amount)
     @sender = sender
