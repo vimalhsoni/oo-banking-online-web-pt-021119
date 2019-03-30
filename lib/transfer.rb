78e7e6cf4eb61311af65d1273b7efcbf443f80c7
Vimal Soni <vimal.h.soni@gmail.com>
2019-03-30 00:22:10 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -35,6 +35,7 @@ def reverse_transfer
   if @status = "complete"
     sender.balance = sender.balance + amount
     receiver.balance = receiver.balance - amount
+    @status = "reversed"
   end
 end
 
