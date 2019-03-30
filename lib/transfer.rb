93d2523ec57ded53ec8b3fdb1227bf8937afbeef
Vimal Soni <vimal.h.soni@gmail.com>
2019-03-30 00:20:08 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -24,8 +24,8 @@ sender.balance = sender.balance - amount
 receiver.balance = receiver.balance + amount
 @status = "complete"
 if sender.valid? == false
-  return "Transaction rejected. Please check your account balance."
   @status = "rejected"
+  return "Transaction rejected. Please check your account balance."
 # receiver.balance
   end
 end
