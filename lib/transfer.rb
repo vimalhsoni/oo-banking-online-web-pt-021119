70826eee836dee0479e655c49588190cbfccd1fa
Vimal Soni <vimal.h.soni@gmail.com>
2019-03-30 00:28:38 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,6 +19,8 @@ end
 def execute_transaction
   # binding.pry
 #need to transfer from the sender account to the receiver account
+if @status == "complete"
+  sender.balance
 if sender.valid? == true
 sender.balance = sender.balance - amount
 receiver.balance = receiver.balance + amount
@@ -26,13 +28,8 @@ receiver.balance = receiver.balance + amount
 if sender.valid? == false
   @status = "rejected"
   return "Transaction rejected. Please check your account balance."
-if @status == "complete"
-  sender.balance
-  receiver.balance
-# receiver.balance
     end
   end
-end
 end
 
 def reverse_transfer
